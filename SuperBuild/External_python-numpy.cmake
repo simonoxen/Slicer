4c71d436c7d2c77ae613a17676638682d0359e2b
Simon Oxenford <oxenfordsimon@gmail.com>
2023-11-09 15:55:59 +0100

    COMP: change numpy and scipy ver

--- SuperBuild/External_python-numpy.cmake
+++ SuperBuild/External_python-numpy.cmake
@@ -28,24 +28,7 @@ endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
   set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
-  file(WRITE ${requirements_file} [===[
-  # [nose]
-  nose==1.3.7 --hash=sha256:9ff7c6cc443f8c51994b34a667bbcf45afd6d945be7477b52e97516fd17c53ac  # needed for NumPy unit tests
-  # [/nose]
-  # [numpy]
-  # Hashes correspond to the following packages:
-  #  - numpy-1.23.4-cp39-cp39-macosx_10_9_x86_64.whl
-  #  - numpy-1.23.4-cp39-cp39-macosx_11_0_arm64.whl
-  #  - numpy-1.23.4-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
-  #  - numpy-1.23.4-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-  #  - numpy-1.23.4-cp39-cp39-win_amd64.whl
-  numpy==1.23.4 --hash=sha256:c67b833dbccefe97cdd3f52798d430b9d3430396af7cdb2a0c32954c3ef73894 \
-                --hash=sha256:f76025acc8e2114bb664294a07ede0727aa75d63a06d2fae96bf29a81747e4a7 \
-                --hash=sha256:12ac457b63ec8ded85d85c1e17d85efd3c2b0967ca39560b307a35a6703a4735 \
-                --hash=sha256:95de7dc7dc47a312f6feddd3da2500826defdccbc41608d0031276a24181a2c0 \
-                --hash=sha256:f260da502d7441a45695199b4e7fd8ca87db659ba1c78f2bbf31f934fe76ae0e
-  # [/numpy]
-  ]===])
+  file(WRITE ${requirements_file} numpy==1.19.3)
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
@@ -54,7 +37,7 @@ if(NOT Slicer_USE_SYSTEM_${proj})
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${requirements_file}
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}
--- SuperBuild/External_python-scipy.cmake
+++ SuperBuild/External_python-scipy.cmake
@@ -28,21 +28,7 @@ endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
   set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
-  file(WRITE ${requirements_file} [===[
-  # [scipy]
-  # Hashes correspond to the following packages:
-  #  - scipy-1.9.2-cp39-cp39-macosx_10_9_x86_64.whl
-  #  - scipy-1.9.2-cp39-cp39-macosx_12_0_arm64.whl
-  #  - scipy-1.9.2-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
-  #  - scipy-1.9.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-  #  - scipy-1.9.2-cp39-cp39-win_amd64.whl
-  scipy==1.9.2 --hash=sha256:1e3b23a82867018cd26255dc951789a7c567921622073e1113755866f1eae928 \
-               --hash=sha256:82e8bfb352aa9dce9a0ffe81f4c369a2c87c85533519441686f59f21d8c09697 \
-               --hash=sha256:61b95283529712101bfb7c87faf94cb86ed9e64de079509edfe107e5cfa55733 \
-               --hash=sha256:8c8c29703202c39d699b0d6b164bde5501c212005f20abf46ae322b9307c8a41 \
-               --hash=sha256:7b2608b3141c257d01ae772e23b3de9e04d27344e6b68a890883795229cb7191
-  # [/scipy]
-  ]===])
+  file(WRITE ${requirements_file} scipy==1.9.2)
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
@@ -51,7 +37,7 @@ if(NOT Slicer_USE_SYSTEM_${proj})
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${requirements_file}
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}
